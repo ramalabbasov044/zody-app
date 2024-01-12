1c5971f93591ace47a5da2349c8145ed646b254a
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-01-12 17:27:15 +0400

    add

--- pages/Help/index.jsx
+++ pages/Help/index.jsx
@@ -1,16 +1,75 @@
-import Head from 'next/head'
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import Footer from '@/shared/components/Footer'
+import Header from '@/shared/components/Header'
 
+import styles from './style.module.css'
+import Head from 'next/head'
+import React, { useState } from 'react'
 const Help = () => {
-  return (
-    <div>
-      <Head>
-        <title>
-          Help
-        </title>
-      </Head>
-    </div>
-  )
+    const [activeTab,setActiveTab] = useState(true)
+    console.log(activeTab);
+
+    return (
+      <div className={styles.Wrapper}>
+        <Head>
+          <title>
+            Help Page
+          </title>
+        </Head>
+
+        <img 
+            src="/help/bg2.svg" 
+            alt="background" 
+            className={styles.BackgroundImage}
+        />
+
+        <Header />
+
+        <main className={styles.Main}>
+            <img 
+                src="/help/bg.svg" 
+                alt="Background" 
+                className={styles.BackgroundImage2}
+            />
+            <div className={styles.Container}>
+                <div className={styles.Content}>
+                    <div className={styles.Tabs}>
+                        <p onClick={() => setActiveTab(true)} className={`${styles.tabItem} ${activeTab ? `${styles.active}` : ""}`}>
+                            Privacy and Policy
+                        </p>
+                        <p onClick={() => setActiveTab(false)} className={`${styles.tabItem} ${activeTab ? "" : `${styles.active}`}`}>
+                            Term and conditions
+                        </p>
+                    </div>
+
+                    <div className={styles.Border}>
+
+                    </div>
+
+                    <div className={styles.tabAbout}>
+                        <p className={styles.tabAboutTitle}>
+                            {
+                              activeTab ? (
+                                  "Gizliliğiniz önceliğimizdir. Uçtan uca şifreleme ile kişisel mesajlarınızın sizinle gönderdiğiniz kişiler arasında kaldığından emin olabilirsiniz. Ne paylaşacağınızı, internette nasıl görüneceğinizi ya da sizinle kimin konuşabileceğini seçebilirsiniz."
+                                ) : (
+                                  <>
+                                    Gizliliğiniz önceliğimizdir. Uçtan uca şifreleme ile kişisel mesajlarınızın sizinle gönderdiğiniz kişiler arasında kaldığından emin olabilirsiniz. Ne paylaşacağınızı, internette nasıl görüneceğinizi ya da sizinle kimin konuşabileceğini seçebilirsiniz.
+                                    <br />
+                                    <br />
+                                    <br />
+                                    Gizliliğiniz önceliğimizdir. Uçtan uca şifreleme ile kişisel mesajlarınızın sizinle gönderdiğiniz kişiler arasında kaldığından emin olabilirsiniz. Ne paylaşacağınızı, internette nasıl görüneceğinizi ya da sizinle kimin konuşabileceğini seçebilirsiniz.
+                                  </>
+                                )
+                              }
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <Footer />
+      </div>
+    )
 }
 
 export default Help
--- shared/components/Header/index.jsx
+++ shared/components/Header/index.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
+import { Routes } from '../../constants/Routes/index'
 import Button from '../Button'
-import { useState } from 'react'
+
 import { useTranslation } from 'react-i18next'
+import { useRouter } from 'next/router'
 import styles from './style.module.css'
+import { useState } from 'react'
+
 const Header = () => {
+    const { push } = useRouter()
     const [isActive, setIsActive] = useState(false);
     const { t, i18n } = useTranslation();
     const activeLanguage = i18n.language; 
@@ -27,8 +32,8 @@ const Header = () => {
                     src="/logo/main.svg"
                     alt='Header Logo'
                     className={styles.Logo}
+                    onClick={() => push("/")}
                 />
-                 
               </div>
 
               <div className={styles.NavbarBody}>
@@ -41,7 +46,7 @@ const Header = () => {
                         </a>
                       </li>
                       <li className={styles.List}>
-                        <a className={styles.Item}>
+                        <a onClick={() => push(Routes.Help)} className={styles.Item}>
                             {
                                 t('helpCenter')
                             }
@@ -117,123 +122,3 @@ const Header = () => {
 }
 
 export default Header
-
-// const Wrapper = styled.header`
-//     position: relative;
-//     z-index: 2;
-//     display: flex;
-//     justify-content: center;
-
-//     .dropdownActive{
-//         background: #A67DFF !important;
-//         height: 119px !important;
-//         padding: 4px !important;
-//     }
-
-//     .languageActiveItem{
-//         padding: 0px 6px;
-//     }
-
-//     .itemsActive{
-//         top: 35px;
-//         padding: 0px 6px;
-//     }
-// `
-
-// const Container = styled.div`
-//     padding: 4px 116px 8px 116px;
-//     display: flex;
-//     justify-content: space-between;
-//     align-items: center;
-//     max-width: 1512px;
-//     width: 100%;
-// `
-
-// const LogoBody = styled.div`
-//     width: 70px;
-//     height: 68px;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-// `
-
-// const Logo = styled.img`
-  
-// `
-
-// const NavbarBody = styled.div`
-//     display: flex;
-//     gap: 184px;
-//     max-height: 44px;
-// `
-
-// const Navbar = styled.div`
-//     display: flex;
-//     gap: 40px;
-// `
-
-// const List = styled.li`
-//     list-style-type: none;
-//     padding: 10px;
-// `
-
-// const Item = styled.a`
-//     color: #FFF;
-//     font-family: Poppins;
-//     font-size: 16px;
-//     font-style: normal;
-//     font-weight: 500;
-//     line-height: normal;
-//     cursor: pointer;
-// `
-
-// const Right = styled.div`
-//     display: flex;
-//     gap: 22px;
-// `
-
-// const LanguageDropdown = styled.div`
-//     position: relative;
-//     width: 72px;
-//     padding: 10px;
-//     display: flex;
-//     justify-content: space-between;
-//     border-radius: 10px;
-//     transition: 1s;
-//     cursor: pointer;
-//     height: 44px;
-    
-// `
-
-// const LanguageActiveItem = styled.div`
-//     width: 100%;
-//     display: flex;
-//     justify-content: space-between;
-// `
-
-// const ActiveTitle = styled.p`
-//     color: #FFF;
-//     font-size: 16px;
-//     font-style: normal;
-//     font-weight: 500;
-//     line-height: normal;
-//     font-family: Poppins;
-// `
-
-// const LanguageItems = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     gap: 5px;
-//     position: absolute;
-//     z-index: 2;
-// `
-
-// const LanguageItem = styled.p`
-//     color: #FFF;
-//     font-size: 16px;
-//     font-style: normal;
-//     font-weight: 500;
-//     line-height: normal;
-//     font-family: Poppins;
-//     cursor: pointer;
-// `
