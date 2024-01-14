5a12baf32e801c7fae200d6e3a44d496e8436332
Ramal Abbassov <ramalabbasov044@gmail.com>
2024-01-14 12:54:42 +0400

    fixed AOS bug

--- pages/Features/index.jsx
+++ pages/Features/index.jsx
@@ -17,8 +17,8 @@ const Features = () => {
              Features Page
           </title>
             <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-            <script defer src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-            <script defer>
+            <script  src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+            <script >
                 AOS.init()
             </script>
         </Head>
